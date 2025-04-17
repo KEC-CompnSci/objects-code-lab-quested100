dcdbf7d7426427d06aaaa9cb64b2b56cffa563bd
quested100 <25rupalk@retsdstudent.ca>
2025-04-17 15:49:29 +0000

    final

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -2,5 +2,29 @@ import java.util.Scanner;
 
 
 public class Question {
+    private String question;
+    public int totalMarks;
+    private String answer;
+    private String studentAnswer;
+
+    public Question(String question,int totalMarks, String answer){
+        this.question = question;
+        this.totalMarks = totalMarks;
+        this.answer = answer;
+
+    }
+    public String AskQuestion(Scanner scanner){
+        System.out.println(this.question + "("+ this.totalMarks + " marks)");
+        this.studentAnswer = scanner.nextLine();
+        return this.studentAnswer;
+    }
+    public int CheckAnswer(){
+        if (this.studentAnswer.equals(this.answer)){
+            return this.totalMarks;
+        }
+        else{
+            return 0;
+        }
+    }
   
 }
